e952c98dd2e40e73e150243bd589400626f267a8
Strakhov Nicholas <strahovnikolaj27@gmail.com>
2026-03-16 20:08:25 +0300

    first commit

---
+++ src/test/java/ru/productallergen/productallergentbank/ProductAllergenTBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.productallergen.productallergentbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAllergenTBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
