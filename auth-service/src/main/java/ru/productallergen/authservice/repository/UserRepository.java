2271d20542d72357b64f887a5235e4d42435219c
Strakhov Nicholas <strahovnikolaj27@gmail.com>
2026-04-18 14:04:31 +0300

    feat: updated yml

--- auth-service/src/main/java/ru/productallergen/authservice/repository/UserRepository.java
+++ auth-service/src/main/java/ru/productallergen/authservice/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package ru.productallergen.authservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.productallergen.authservice.entity.User;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
